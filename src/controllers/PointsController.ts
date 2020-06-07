e54c021a1a08c2b6cac59745fd343a56553e1860
FelipeMaxRosa <felipemax.suporteti@gmail.com>
2020-06-07 16:40:57 -0300

    implemented insertion of the image link fake as example

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -16,7 +16,7 @@ class PointsController {
   
     const trx = await knex.transaction();
     const point = {
-      image: 'image-fake', 
+      image: 'https://images.unsplash.com/photo-1591183843088-8ea6a3470843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60', 
       name, 
       email, 
       whatsapp, 
